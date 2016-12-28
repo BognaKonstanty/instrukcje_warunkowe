396e9d849d171abe043ed6aeef9cf80593262acc
Bogna Konstanty <rosiakb@gmail.com>
2016-12-28 10:26:13 +0100

    modified scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,12 @@
 var a = 1;
 var b = 9;
 var value = (a*a) + (2 * a * b) - (b*b);
-console.log(' Wynik: ' + value);
 
 if (value > 0) {
-	console.log('Wynik jest dodatni');
+	console.log(' Wynik wynosi: ' + value+' Wynik jest dodatni');
+}else if (value < 0) {
+	console.log(' Wynik wynosi: ' + value+' Wynik jest ujemny');
+}else  {
+	console.log(' Wynik wynosi: ' + value+' Wynik jest rowny zero');
 }
 
-else if (value < 0) {
-	console.log('Wynik jest ujemny');
-}
-
-
-else  {
-	console.log('Wynik jest rowny zero');
-}
